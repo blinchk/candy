6951888ad78969facceba3acd1e0206ec43e3474
Nikolas Laus <bchik3@gmail.com>
2021-02-02 00:16:02 +0200

    Redefine GameModes enum.

--- Osu.Candy/GameInfo/GameModes.cs
+++ Osu.Candy/GameInfo/GameModes.cs
@@ -2,9 +2,9 @@ namespace Osu.Candy.GameInfo
 {
     public enum GameModes
     {
-        Fruits = 1,
-        Mania = 2,
-        Standard = 3,
-        Taiko = 4
+        Standard = 0,
+        Taiko = 1,
+        Catch = 2,
+        Mania = 3
     }
 }
