86ae30050989f1e0de2feaa3410f2fdd9fcc85c9
Nikolas Laus <bchik3@gmail.com>
2021-02-01 05:44:19 +0200

    Move whole project to ASP.NET

---
+++ Osu.Candy/ClientApp/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.scss';
+import React, { Component } from 'react';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
+
+import logo from './assets/img/logo.png';
+
+function App() {
+    return (
+        <div className="app">
+            <Header />
+            <Main />
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
