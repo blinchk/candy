44c400e4080987a48c2a4ed08c7f62ffbd9ef99b
Nikolas Laus <bchik3@gmail.com>
2021-02-01 06:22:58 +0200

    Fix PerformancePoints typo in prop name.

---
+++ Osu.Candy/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Osu.Candy.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Nickname { get; set; }
+        public string CountryCode { get; set; }
+        public DateTime JoinDate { get; set; }
+        public int Rating { get; set; }
+        public int PerformancePoints { get; set; }
+        public int GlobalRank { get; set; }
+        public int CountryRank { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsOnline { get; set; }
+        public bool AvatarUrl { get; set; }
+    }
+}
